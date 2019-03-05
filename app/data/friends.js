d41703e41220176c8364bf7a7e101aad82941c82
kietosstarik <44623576+kietosstarik@users.noreply.github.com>
2019-03-04 18:23:25 -0600

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+    {
+    "name": "Pikachu",
+    "photo": "https://i.redd.it/xmulu1f6urt11.jpg",
+    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  }, {
+    "name": "Dragon",
+    "photo": "https://i.ytimg.com/vi/OSI102cY3sE/maxresdefault.jpg",
+    "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  }, {
+    "name": "Pablo Escobar",
+    "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTM5MjE5MzAzODUyNDE4Mzk5/pablo-escobar-gettyimages-452490135_621jpg.jpg",
+    "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  }, {
+    "name": "Peter Jackson",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Peter_Jackson_SDCC_2014.jpg/220px-Peter_Jackson_SDCC_2014.jpg",
+    "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  }, {
+    "name": "Mo Bamba",
+    "photo": "https://cdn.vox-cdn.com/thumbor/4j94bxMm9NER_U0wzR2x-GhLrFE=/0x0:3000x1988/1200x800/filters:focal(1178x525:1658x1005)/cdn.vox-cdn.com/uploads/chorus_image/image/60143577/899651886.jpg.0.jpg",
+    "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  }, {
+    "name": "Tsar Nicholar II",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/%D0%98%D0%BC%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9_II.jpg/220px-%D0%98%D0%BC%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9_II.jpg",
+    "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  }];
+  
+  module.exports = friends;
